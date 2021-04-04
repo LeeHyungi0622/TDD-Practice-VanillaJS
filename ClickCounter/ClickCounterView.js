1f3d246113746864db6e88737ec76583937abd34
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-04-04 15:39:41 +0900

    feat: Write the code to pass DI(Dependency Injection) test cases

--- ClickCounter/ClickCounterView.js
+++ ClickCounter/ClickCounterView.js
@@ -1,6 +1,8 @@
 var App = App || {};
 
 App.ClickCounterView = (clickCounter, updateEl) => {
+    if (!clickCounter) throw Error('clickCounter');
+    if (!updateEl) throw Error('updateEl');
     return {
         updateView() {
             updateEl.innerHTML = clickCounter.getValue();
